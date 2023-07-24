b196aead1a9e8dcb0f9a2f7ab1f6a3b4904ee512
oleksiilunin <116845999+oleksiilunin@users.noreply.github.com>
2023-07-24 03:51:35 +0300

    Add "sort" to addContact. Some fix

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,34 +1,33 @@
-import { createSlice, nanoid } from "@reduxjs/toolkit"
-
-const contactsInitialState = [
-  { id: 0, name: "AAAAAA", number: '111111111111' },
-  { id: 1, name: "BBBBBB", number: '222222222' },
-  { id: 2, name: "CCCCCCCCCCCC", number: '33333333333' },
-  { id: 3, name: "DDDDDDddDDD DD", number: '444444444' },
-  { id: 4, name: "EEEE Eeeeeee", number: '55555555555' },
-];
+import { createSlice, nanoid } from '@reduxjs/toolkit';
 
 const contactsSlice = createSlice({
-	name: 'contacts',
-	initialState: contactsInitialState,
-	reducers: {
-		addContact: {
-			reducer(state, action) {
-				state.push(action.payload);
-			},
-			prepare(valueObj) {
-				return {
-					...valueObj,
-					id: nanoid(),
-				}
-			}
-		},
+  name: 'contacts',
+  initialState: [],
+  reducers: {
+    addContact: {
+      reducer(state, action) {
+        state.push(action.payload);
+        state.sort((firstContact, secondContact) =>
+          firstContact.name
+            .toLowerCase()
+            .localeCompare(secondContact.name.toLowerCase())
+        );
+      },
+      prepare(valueObj) {
+        return {
+          payload: {
+            ...valueObj,
+            id: nanoid(),
+          },
+        };
+      },
+    },
 
-		deleteContact(state, action) {
-			const index = state.findIndex(contact => contact.id === action.payload);
-			state.splice(index, 1);
-		},
-	},
+    deleteContact(state, action) {
+      const index = state.findIndex(contact => contact.id === action.payload);
+      state.splice(index, 1);
+    },
+  },
 });
 
 export const { addContact, deleteContact } = contactsSlice.actions;
