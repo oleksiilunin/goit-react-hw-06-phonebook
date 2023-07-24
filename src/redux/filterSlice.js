17775e326138af4ede62d554e7bf57f92b1da988
oleksiilunin <116845999+oleksiilunin@users.noreply.github.com>
2023-07-24 03:50:05 +0300

    Add "return" to reducer function

--- src/redux/filterSlice.js
+++ src/redux/filterSlice.js
@@ -1,13 +1,13 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 const filterSlice = createSlice({
-	name: 'filter',
-	initialState: '',
-	reducers: {
-		setFilterValue(state, action) {
-			state.filter = action.payload;
-		},
-	},
+  name: 'filter',
+  initialState: '',
+  reducers: {
+    setFilterValue(state, action) {
+      return (state = action.payload);
+    },
+  },
 });
 
 export const { setFilterValue } = filterSlice.actions;
